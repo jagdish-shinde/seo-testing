a9f34ae039935043c440c049da2fdfe10478f96d
jaGdish <jagdish.shinde@functionup.org>
2023-10-25 16:33:33 +0530

    fix : generic page ui

--- components/atoms/whatsapp-community-btn/whatsapp-community-btn.js
+++ components/atoms/whatsapp-community-btn/whatsapp-community-btn.js
@@ -4,7 +4,7 @@ import styles from "./whatsapp-community-btn.module.css"
 import { WHATSAPP_COMMUNITY_GROUP_LINK } from "../../../util/constants"
 import * as amplitude from '@amplitude/analytics-browser';
 
-export default function WhatsappCommunityBtn({pageTitle, collegeName='', degreeName=''}) {
+export default function WhatsappCommunityBtn({pageTitle, collegeName='', degreeName='',containerStyle}) {
     function handleJoinWhatsapp(){
         amplitude.track('SEO_JOINED_WA_COMMUNITY', {
             pageTitle,
@@ -15,7 +15,7 @@ export default function WhatsappCommunityBtn({pageTitle, collegeName='', degreeN
         window.open(WHATSAPP_COMMUNITY_GROUP_LINK, "_blank")
     }
   return (
-    <div className={styles.mainContainer}>
+    <div className={`${styles.mainContainer} ${containerStyle}`}>
         <div className={styles.btnWrapper} onClick={handleJoinWhatsapp}>
             <button className={styles.btn}>
                 Join for Exam Updates
