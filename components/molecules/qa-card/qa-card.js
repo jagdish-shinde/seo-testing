efe263e4bae75a9c45689b943c0d26b83488735c
jaGdish <jagdish.shinde@functionup.org>
2023-10-25 19:48:16 +0530

    fix : list rich content

--- components/molecules/qa-card/qa-card.js
+++ components/molecules/qa-card/qa-card.js
@@ -6,8 +6,6 @@ import HtmlParser from 'react-html-parser';
 export function QaCard ({data}) {
     const [showFullText, setShowFullText] = useState(false);
     const [showFullTextDekstop, setShowFullTextDekstop] = useState(false)
-    const [isExpandanleTextMobile, setIsExpandanleTextMobile] = useState(false)
-    const [isExpandanleTextDesktop, setIsExpandanleTextDesktop] = useState(false)
 
     let {
         description = '',
@@ -15,26 +13,8 @@ export function QaCard ({data}) {
         title = ''
     } = data|| {}
 
-    const maxCharacterMobile = 55;
-    const maxCharacterDesktop = 150;
-
-    useEffect(() => {
-
-        const descriptionText = HtmlParser(description)?.[0].props?.children?.[0] || description
-        if(descriptionText?.length <= maxCharacterMobile){
-            setShowFullText(true)
-        }else{
-            setIsExpandanleTextMobile(true)
-        }
-        if(descriptionText?.length <= maxCharacterDesktop){
-            setShowFullTextDekstop(true)
-        }else{
-            setIsExpandanleTextDesktop(true)
-        }
-    },[description])
-
     return (
-        <div className={`${styles.wrapper} ${isExpandanleTextDesktop && styles.container}`}>
+        <div className={styles.wrapper}>
 
             <div className={styles.leftSec}>
                 <Image
@@ -53,17 +33,14 @@ export function QaCard ({data}) {
                 <div className={styles.textContaintMobile}>
 
                     <div className={
-                            showFullText && description?.length > maxCharacterMobile ? 
+                            showFullText ? 
                             styles.fullDesc : styles.description
                     }>
                         { 
-                            showFullText ? 
-                            HtmlParser(description) : 
-                            HtmlParser(description?.substring(0, maxCharacterMobile))
+                            HtmlParser(description) 
                         }
                     </div>
                     {
-                        isExpandanleTextMobile && 
                         <span className={styles.viewMore} onClick={()=>setShowFullText(!showFullText)}>
                             {!showFullText ? 'View More' : 'View Less'}
                         </span>
@@ -74,16 +51,13 @@ export function QaCard ({data}) {
                 <div className={styles.textContaintDekstop}>
 
                     <div className={
-                        showFullTextDekstop && description?.length > maxCharacterDesktop ? 
+                        showFullTextDekstop ? 
                         styles.fullDesc : styles.description
                     }>
                         {
-                            showFullTextDekstop ? 
-                            HtmlParser(description) : 
-                            HtmlParser(description?.substring(0, maxCharacterDesktop))
+                            HtmlParser(description) 
                         }
                         {
-                            isExpandanleTextDesktop &&
                             <span className={styles.viewMore} onClick={()=>setShowFullTextDekstop(!showFullTextDekstop)}>
                                 {!showFullTextDekstop ? 'View More' : 'View Less'}
                             </span>
